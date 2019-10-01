f82b76b4f1aeb9a24e4b4ea7e0262a95a72e894e
Phani Saripalli <saripalli.phani@gmail.com>
2019-10-01 17:42:20 +0200

    #EYZ-180 Bulk read, save the zip, extract the csv of a module
    Note: this doesnt get the subforms.

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 "use strict";
 var zcrmsdk = require("zcrmsdk");
 const s3Tokens = require("./token_mgmt")
+const request = require('request')
+
+
+function requestPromise(options) {
+	return new Promise(function (resolve, reject) {
+		request(options, function (error, response, body) {
+			if (error) return reject(error);
+			try {
+				resolve(body);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	});
+}
+
+function execShellCommand(cmd) {
+	const exec = require('child_process').exec;
+	return new Promise((resolve, reject) => {
+		exec(cmd, (error, stdout, stderr) => {
+			if (error) reject(error);			
+			resolve(stdout ? stdout : stderr);
+		});
+	});
+}
 
 /** Zoho class with utility functions */
 class Zoho {
@@ -8,21 +33,30 @@ class Zoho {
 	 * Creates a client to communicate with zoho API.
 	 * @constructor
 	 */
-	constructor() {	}
+	constructor() { }
 
 	async init() { }
 
-	async getClient() {		
-		if (!this.client) await zcrmsdk.initialize();
-		else {
-			let tokenObj = await s3Tokens.getOAuthTokens();			
-			let expirytime = tokenObj[0].expirytime;
-			var ts = Math.round((new Date()).getTime() );			
-			// console.log(ts, expirytime, (ts >= (expirytime - 1000)));
+	async getClient(generate = false) {
+		let tokenObj = await s3Tokens.getOAuthTokens();
+		let expirytime = tokenObj[0].expirytime;
+		let refreshToken = tokenObj[0].refreshtoken;
+		var ts = Math.round((new Date()).getTime());
 
-			if (ts >= (expirytime - 1000)) await zcrmsdk.initialize();
+		if (!this.client) await zcrmsdk.initialize();
+
+		let toInit = ts >= (expirytime - 1000);
+
+		// console.log(ts, expirytime, toInit);
+
+		if (toInit) await zcrmsdk.initialize();
+
+		this.client = zcrmsdk;
+
+		if (toInit && generate) {
+			// console.log("generating");
+			await zcrmsdk.generateAuthTokenfromRefreshToken(null, refreshToken);
 		}
-		this.client = zcrmsdk;		
 
 		return this.client;
 	}
@@ -40,7 +74,7 @@ class Zoho {
 	 * @returns {Object} response Zoho API Response.
 	 * @returns {String} response.body if there are results.
 	 */
-	async getRecords(params) {		
+	async getRecords(params) {
 		if (!params.module) {
 			return { error: true };
 		}
@@ -54,15 +88,15 @@ class Zoho {
 
 		var input = { module: params.module };
 		input.params = { page: page, per_page: per_page, sort_by: sort_by, sort_order: sort_order };
-		
+
 		let response = null;
-		
+
 		if (!params.has_subform) {
 			try {
-				response = await client.API.MODULES.get(input);								
+				response = await client.API.MODULES.get(input);
 				if (response.statusCode != 200) {
 					return { records: [], statusCode: response.statusCode, info: jsonResponse.info };
-				}				
+				}
 				let jsonResponse = JSON.parse(response.body);
 				return { records: jsonResponse.data, statusCode: response.statusCode, info: jsonResponse.info };
 			} catch (error) {
@@ -72,25 +106,25 @@ class Zoho {
 
 		try {
 			let response = await client.API.MODULES.get(input);
-			
+
 			if (!response.body)
 				return { records: [], statusCode: 204 };
 
 			let bodyObj = JSON.parse(response.body);
 			let data = bodyObj.data;
 
-			if(!data) return {records: [], statusCode: 204};
-			
+			if (!data) return { records: [], statusCode: 204 };
+
 			let records = [];
 			let zoho = new Zoho();
 
 			for (let item of data) {
 				let id = item.id;
-				let recordResponse = await zoho.getRecord(params.module, id);				
+				let recordResponse = await zoho.getRecord(params.module, id);
 				let recordData = recordResponse.record;
 				records.push(recordData);
-			}			
-			return {records: records, statusCode: 200, info: bodyObj.info};
+			}
+			return { records: records, statusCode: 200, info: bodyObj.info };
 		} catch (err) {
 			console.log(err);
 			return { error: true, error_details: err };
@@ -110,7 +144,7 @@ class Zoho {
 	 * @returns {List} response.records if there are results.
 	 * @returns {Integer} response.count if there are results.
 	 */
-	async getRecordsModifiedAfter(params) {		
+	async getRecordsModifiedAfter(params) {
 		if (!params.module) {
 			return { error: true };
 		}
@@ -118,22 +152,22 @@ class Zoho {
 		let modifiedAfter = params.modified_after;
 
 		if (!modifiedAfter) return { error: true, records: null };
-		
+
 		let page = params.page ? params.page : 1;
 		let per_page = params.per_page ? params.per_page : 100;
 		let sort_by = "Modified_Time";
-		let sort_order = "desc";		
+		let sort_order = "desc";
 
 		let zoho = new Zoho();
 		let hasMore = true;
 		let resultData = [];
 
-		while(hasMore) {			
-			try {				
+		while (hasMore) {
+			try {
 				let tempParams = { page: page, per_page: per_page, sort_by: sort_by, sort_order: sort_order };
 				Object.assign(params, tempParams);
-				
-				let response = await zoho.getRecords(params);								
+
+				let response = await zoho.getRecords(params);
 
 				if (!response.records) hasMore = false;
 				else {
@@ -146,14 +180,14 @@ class Zoho {
 							hasMore = false;
 							break;
 						}
-					}					
-				}				
+					}
+				}
 				page++;
-			} catch(err) {				
+			} catch (err) {
 				hasMore = false;
 				return { error: true, count: 0, error_details: err };
-			}				
-		}		
+			}
+		}
 
 		return { error: false, records: resultData, count: resultData.length };
 	}
@@ -178,28 +212,176 @@ class Zoho {
 		let page = 1;
 		let per_page = params.per_page ? params.per_page : 100;
 		let sort_by = params.sort_by ? params.sort_by : "Modified_Time";
-		let sort_order = params.sort_order ? params.sort_order : "desc";		
-		
+		let sort_order = params.sort_order ? params.sort_order : "desc";
+
 		let hasMore = true;
 		let resultData = [];
 
-		while(hasMore) {			
-			try {				
+		while (hasMore) {
+			try {
 				let tempParams = { page: page, per_page: per_page, sort_by: sort_by, sort_order: sort_order };
 				Object.assign(params, tempParams);
-				
-				let response = await this.getRecords(params);						
+
+				let response = await this.getRecords(params);
 				if (!response.records && response.records.length > 0) hasMore = false;
 				else {
-					resultData.push(...response.records)					
-				}				
+					resultData.push(...response.records)
+				}
 				page++;
-			} catch(err) {				
-				hasMore = false;				
-			}				
-		}		
+			} catch (err) {
+				hasMore = false;
+			}
+		}
 
-		return {records: resultData, count: resultData.length, statusCode: 200};
+		return { records: resultData, count: resultData.length, statusCode: 200 };
+	}
+
+
+	async bulkRead(id) {
+		await this.getClient();
+
+		let url = `https://www.zohoapis.com/crm/bulk/v2/read/${id}`;
+
+		let tokenObj = await s3Tokens.getOAuthTokens();
+		let accessToken = tokenObj[0].accesstoken;
+
+		var options = {
+			method: 'get',
+			url: url,
+			headers: {
+				Authorization: `Zoho-oauthtoken  ${accessToken}`
+			}
+		}
+		try {
+			let responseS = await requestPromise(options);
+			let response = JSON.parse(responseS);
+
+			if (!response.data) return { success: false, code: response.code };
+			let data = response.data[0];
+
+			return { success: true, data: data };
+		} catch (error) {
+			return { success: false, error: console.error() };
+		}
+	}
+
+	async bulkReadCreate(module) {
+		if (!module) {
+			return { error: true };
+		}
+
+		await this.getClient(true);
+
+		let tokenObj = await s3Tokens.getOAuthTokens();
+		let accessToken = tokenObj[0].accesstoken;
+
+		let url = "https://www.zohoapis.com/crm/bulk/v2/read";
+		let jsonBody = { query: { module: module } };
+
+		var options = {
+			method: 'post',
+			body: jsonBody,
+			json: true,
+			url: url,
+			headers: {
+				Authorization: `Zoho-oauthtoken  ${accessToken}`
+			}
+		}
+
+		try {
+			let response = await requestPromise(options);
+			if (!response.data) return { success: false, code: response.code };
+			let data = response.data[0];
+			let details = data.details;
+
+			return { success: true, details: details };
+		} catch (error) {
+			return { success: false, error: console.error() };
+		}
+	}
+
+	async bulkReadDownload(jobId, destination) {
+		if (!destination.endsWith(".zip")) return { success: false };
+
+		await this.getClient();
+
+		let tokenObj = await s3Tokens.getOAuthTokens();
+		let accessToken = tokenObj[0].accesstoken;
+		console.log(accessToken);
+
+		let url = `https://www.zohoapis.com/crm/bulk/v2/read/${jobId}/result`;
+		let cmd = `curl -o ${destination} "${url}" -X GET -H "Authorization: Zoho-oauthtoken ${accessToken}"`;
+		// console.log(cmd);
+		
+		try {			
+			await execShellCommand(cmd);
+			await execShellCommand(`unzip ${destination}`);
+			console.log(`extracted file - ${jobId}.csv`);
+			return { success: true };
+		} catch (error) {
+			console.log(error);
+			return { success: false };
+		}
+	}
+
+	async _checkStatus(jobId, destination) {
+		var intervalId = null;
+		let zoho = new Zoho();
+		var state = async function () {
+			let resp = await zoho.bulkRead(jobId);
+			let jState = resp.data.state;
+			if (jState == "COMPLETED") {
+				// console.log(jState);				
+				await zoho.bulkReadDownload(jobId, destination);
+			}
+			return jState;
+		}
+
+		return new Promise((resolve, reject) => {
+			intervalId = setInterval(() => {
+				state()
+					.then((data) => {
+						console.log(data);
+						if (!["ADDED", "COMPLETED", "IN PROGRESS", "QUEUED"].includes(data)) {
+							reject(false);
+						} else if (data === 'COMPLETED') {
+							clearInterval(intervalId);
+							resolve(true);
+						}
+					});
+			}, 10000);
+		});
+	}
+
+	async downloadModule(module, destination) {
+		let bulkReadCreateResult = null;
+		let bulkReadResult = null;
+		let jobId = null;
+
+		try {
+			bulkReadCreateResult = await this.bulkReadCreate(module);
+			if (!bulkReadCreateResult.success) return { success: false };
+		} catch (error) {
+			return { success: false };
+		}
+
+		try {
+			jobId = bulkReadCreateResult.details.id;
+			bulkReadResult = await this.bulkRead(jobId);
+			if (!bulkReadResult.data) return { success: false };
+
+		} catch (error) {
+			return { success: false };
+		}
+
+		console.log(`started with job id ${jobId}`);
+
+		try {
+			await this._checkStatus(jobId, destination);
+			return { success: true, jobId: jobId };
+		} catch (err) {
+			return { success: false, jobId: jobId };
+		}
 	}
 
 	/**
@@ -209,16 +391,16 @@ class Zoho {
 	 * @returns {Object} response
 	 * @returns {Object} response.record if there is a record with the id
 	 */
-	async getRecord(module, id) {				
+	async getRecord(module, id) {
 		var input = { module: module, id: id };
 		let client = await this.getClient();
 		try {
-			let response = await client.API.MODULES.get(input);			
-			if (response.body) return {record: JSON.parse(response.body).data[0]}
+			let response = await client.API.MODULES.get(input);
+			if (response.body) return { record: JSON.parse(response.body).data[0] }
 			else return {}
 
 		} catch (error) {
-			return {error: true, error_details: error};
+			return { error: true, error_details: error };
 		}
 	}
 
@@ -229,19 +411,19 @@ class Zoho {
 	 * @param {Array} data Array of objects, e.g. [{apiname:value, apiname2:value2, ...}]
 	 * @returns {Object} response
 	 */
-	async updateRecord(module, id, data) {		
+	async updateRecord(module, id, data) {
 		var input = { module: module, id: id };
 		input.body = { data: data };
 
 		let client = await this.getClient();
 		return client.API.MODULES.put(input)
-			.then(function(response) {				
+			.then(function (response) {
 				if (response.body) return JSON.parse(response.body);
 				return {};
 			})
-			.catch(function(err) {
+			.catch(function (err) {
 				console.log(err)
-				return {error: err};
+				return { error: err };
 			});
 	}
 
@@ -254,16 +436,16 @@ class Zoho {
 	 * @returns {Object} response
 	 * @returns {String} response.status success | error
 	 */
-	async insertRecord(module, data) {		
-		var input = { module: module};
+	async insertRecord(module, data) {
+		var input = { module: module };
 		input.body = { data: data };
 
 		let client = await this.getClient();
 		return client.API.MODULES.post(input)
-			.then(function(response) {
+			.then(function (response) {
 				return response;
 			})
-			.catch(function(err) {
+			.catch(function (err) {
 				return err;
 			});
 	}

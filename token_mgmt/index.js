5149fe959a0ff0df760e633f250b2c28fcfb777b
Phani Saripalli <saripalli.phani@gmail.com>
2020-05-24 14:45:33 +0200

    eyz-744 bug fix when uploading wrong token to s3 and minor version bump

--- token_mgmt/index.js
+++ token_mgmt/index.js
@@ -3,20 +3,35 @@ var tokens = require("./s3_tokens");
 const expiryInterval = 3600;
 exports.expiryInterval = expiryInterval;
 
+const verifyToken = (tokenObj) => {
+	// if (
+	// 	Object.prototype.hasOwnProperty.call(tokenObj, "access_token") &&
+	// 	Object.prototype.hasOwnProperty.call(tokenObj, "expires_in") &&
+	// 	Object.prototype.hasOwnProperty.call(tokenObj, "refresh_token")
+	// ) {
+	// 	return true;
+	// }
+
+	if (tokenObj.hasOwnProperty("access_token") && tokenObj.hasOwnProperty("expires_in") && tokenObj.hasOwnProperty("refresh_token")) {
+		return true;
+	}
+	return false;
+};
+
 /**
  * Save token to s3. This function is called by zoho with the token object.
  */
 exports.saveOAuthTokens = async (tokenObj) => {
-	let validKeys = ["access_token", "expires_in", "refresh_token"];
-	for (let validKey of validKeys) {
-		if (!tokenObj[validKey]) {
-			throw new Error(`saveOAuthTokens: invalid tokenObject received for client ${process.env.CRM_CLIENT_ID} tokenObj ${JSON.stringify(tokenObj)}`);
-		}
-	}
-
+	console.log('saveOAuthTokens', tokenObj);
+	
 	try {
-		await tokens.setTokens(tokenObj);
-		return tokenObj;
+		if (verifyToken(tokenObj)) {
+			await tokens.setTokens(tokenObj);
+			return tokenObj;
+		} else {
+			console.log("recieved token is invalid in saveOAuthTokens");
+			return {};
+		}		
 	} catch (err) {
 		console.log("problem writing tokens");
 		console.log(err);
@@ -29,27 +44,16 @@ exports.saveOAuthTokens = async (tokenObj) => {
  * This function is called by zoho with the token object.
  */
 exports.updateOAuthTokens = async (tokenObj) => {
-	/*
-	Updates dont have refresh_token. So, we take the one stored in s3 and update the zoho token object with it.
-	And further check if the token object has all the keys and their values are not null
-	*/
-	if (!tokenObj.refresh_token) {
-		let tokenBody = await tokens.getTokens();
-		let token = JSON.parse(tokenBody.Body.toString());
-		let oldRefreshToken = token["refresh_token"];
-		tokenObj["refresh_token"] = oldRefreshToken;
-	}
-
-	let validUpdateKeys = ["access_token", "expires_in", "refresh_token"];
-	for (let validKey of validUpdateKeys) {
-		if (!tokenObj[validKey]) {
-			throw new Error(`updateOAuthTokens: invalid tokenObject received for client ${process.env.CRM_CLIENT_ID} tokenObj ${JSON.stringify(tokenObj)}`);
-		}
-	}
+	console.log(tokenObj, 'tokenObj');
 
 	try {
-		await tokens.setTokens(tokenObj);		
-		return tokenObj;
+		if (verifyToken(tokenObj)) {
+			await tokens.setTokens(tokenObj);		
+			return tokenObj;
+		} else {
+			console.log("recieved token is invalid in updateOAuthTokens");
+			return {};
+		}
 	} catch (err) {
 		console.log("problem writing tokens");
 		console.log(err);
@@ -61,10 +65,11 @@ exports.updateOAuthTokens = async (tokenObj) => {
  * Fetch the token. This function is called by Zoho.
  */
 exports.getOAuthTokens = async (userIdentifier) => {
+	console.log('getOAuthTokens');
 	let tokenBody = await tokens.getTokens();
 	let token = JSON.parse(tokenBody.Body.toString());
-
-	//console.log(`token will expire at ${token.expires_in}`);
+	
+	console.log('returning token from s3', token);
 
 	return new Promise(function(resolve, reject) {
 		resolve(token);
